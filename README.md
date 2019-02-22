33f42e0addd534d2c39a22d858c29476d8d6e8b9
George Cordalis <cordalis.g@gmail.com>
2019-02-22 20:55:53 +1300

    Version 0.0.1 Adds basic functionality to the Home app. - Power State + Toggle - Input State + Selection - Initial support for volume + remote control*
    *pending iOS 12.2 beta fixes

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # homebridge-onkyo-tv
+
+`homebridge-onkyo-tv` is a plugin for HomeBridge which allows you to control your Onkyo Receiver! It should work with most recent Onkyo Receivers (2016+) however it has only been tested with the TX-NR575E.
+This provides native HomeKit control to the Onkyo Receiver.
+
+### Features
+
+- HomeKit TV integration
+- Power status
+- Turn On / Off
+- Input Selection
+
+### Coming Soon
+
+- Volume Control
+- Mute / Unmute
+
+**NOTE:** This module only works with iOS 12.2 and later. iOS 12.2 is currently in beta and some features/functionality may change.
+
+## Installation
+
+If you are new to Homebridge, please first read the Homebridge [documentation](https://www.npmjs.com/package/homebridge).
+
+Install homebridge:
+
+```sh
+sudo npm install -g homebridge
+```
+
+Install homebridge-webos-tv:
+
+```sh
+sudo npm install -g homebridge-onkyo-tv
+```
+
+## Configuration
+
+Add the accessory in `config.json` in your home directory inside `.homebridge`.
+
+Example configuration:
+
+```js
+{
+  "accessories": [
+    {
+      "accessory": "tvservice-onkyo",
+      "name": "TX-NR575E",
+      "ip": "10.0.0.132",
+      "volumeLimit": 75
+    }
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,524 @@
+var Service, Characteristic, VolumeCharacteristic;
+var eiscp = require("eiscp");
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory(
+    "homebridge-tvservice-onkyo",
+    "tvservice-onkyo",
+    onkyoAccessory
+  );
+};
+
+function onkyoAccessory(log, config) {
+  // Variables received from config.json
+  this.name = config["name"];
+  this.host = config["ip"];
+  this.volumeLimit = config["volumeLimit"] || 100;
+
+  // Set variables
+  this.log = log;
+  this.connected = false;
+  this.muteState = false;
+  this.powerState = false;
+  this.tvVolume = 0;
+  this.currentInput = "";
+
+  var that = this;
+  this.services = [];
+
+  // Preset variables for eiscp module
+  // Setting verify_commands to false allows the module to send raw commands as is to the receiver regardless of if the command is supported
+  // host forces the module to connect to a specific receiver
+  this.eiscpConfig = {
+    verify_commands: false,
+    host: this.host
+  };
+
+  this.service = new Service.Television(this.name, "service");
+  this.service.setCharacteristic(Characteristic.ConfiguredName, this.name);
+  this.services.push(this.service);
+
+  this.inputService = new Service.InputSource(this.name);
+
+  this.speakerService = new Service.TelevisionSpeaker(
+    this.name + " Volume",
+    "volumeService"
+  );
+
+  this.speakerService
+    .setCharacteristic(Characteristic.Active, Characteristic.Active.ACTIVE)
+    .setCharacteristic(
+      Characteristic.VolumeControlType,
+      Characteristic.VolumeControlType.ABSOLUTE
+    );
+
+  this.speakerService
+    .getCharacteristic(Characteristic.VolumeSelector)
+    .on("set", function(code, callback) {
+      callback(false);
+    });
+
+  this.services.push(this.speakerService);
+
+  // All inputs for the Onkyo TX-NR575E
+  this.defaultInputs = [
+    {
+      code: "SLI01",
+      name: "CBL / SAT",
+      type: Characteristic.InputSourceType.HDMI
+    },
+    {
+      code: "SLI02",
+      name: "GAME",
+      type: Characteristic.InputSourceType.HDMI
+    },
+    {
+      code: "SLI03",
+      name: "AUX",
+      type: Characteristic.InputSourceType.HDMI
+    },
+    {
+      code: "SLI05",
+      name: "PC",
+      type: Characteristic.InputSourceType.HDMI
+    },
+    {
+      code: "SLI10",
+      name: "BD / DVD",
+      type: Characteristic.InputSourceType.HDMI
+    },
+    {
+      code: "SLI11",
+      name: "STRM BOX",
+      type: Characteristic.InputSourceType.HDMI
+    },
+    {
+      code: "SLI22",
+      name: "PHONO",
+      type: Characteristic.InputSourceType.AUDIO_SYSTEM
+    },
+    {
+      code: "SLI23",
+      name: "CD",
+      type: Characteristic.InputSourceType.AUDIO_SYSTEM
+    },
+    {
+      code: "SLI24",
+      name: "FM Radio",
+      type: Characteristic.InputSourceType.TUNER
+    },
+    {
+      code: "SLI25",
+      name: "AM Radio",
+      type: Characteristic.InputSourceType.TUNER
+    },
+    {
+      code: "SLI29",
+      name: "USB",
+      type: Characteristic.InputSourceType.OTHER
+    },
+    {
+      code: "SLI2B",
+      name: "NETWORK",
+      type: Characteristic.InputSourceType.OTHER
+    }
+  ];
+
+  this.inputAppIds = new Array();
+  // predefined inputs
+  this.defaultInputs.forEach((value, i) => {
+    let tmpDefaultSource = new Service.InputSource(
+      value.name,
+      "inputSource" + i
+    );
+    tmpDefaultSource
+      .setCharacteristic(Characteristic.Identifier, i)
+      .setCharacteristic(Characteristic.ConfiguredName, value.name)
+      .setCharacteristic(
+        Characteristic.IsConfigured,
+        Characteristic.IsConfigured.CONFIGURED
+      )
+      .setCharacteristic(Characteristic.InputSourceType, value.type)
+      .setCharacteristic(
+        Characteristic.CurrentVisibilityState,
+        Characteristic.CurrentVisibilityState.SHOWN
+      );
+
+    this.service.addLinkedService(tmpDefaultSource);
+    this.services.push(tmpDefaultSource);
+    this.inputAppIds.push(value.code);
+  });
+
+  this.service
+    .getCharacteristic(Characteristic.ActiveIdentifier)
+    .on("set", (code, callback) => {
+      if (this.connected) {
+        this.log("onkyo - Input Source", this.inputAppIds[code]);
+        eiscp.raw(this.inputAppIds[code], data => {
+          this.currentInput = this.inputAppIds[code];
+        });
+        callback();
+      } else {
+        callback(null, false);
+      }
+    })
+    .on("get", callback => {
+      if (this.connected) {
+        eiscp.raw("SLIQSTN", data => {
+          for (input in this.defaultInputs) {
+            if (this.defaultInputs[input].code === data.iscp_command) {
+              this.currentInput = this.defaultInputs[input].code;
+            }
+          }
+          callback(null, this.currentInput);
+        });
+      } else if (!this.currentInput === "") {
+        callback(null, this.currentInput);
+      } else {
+        callback(null, false);
+      }
+    });
+
+  this.prepareInformationService();
+
+  this.service
+    .getCharacteristic(Characteristic.Active)
+    .on("get", this.getPowerState.bind(this))
+    .on("set", this.setPowerState.bind(this));
+
+  eiscp.connect(this.eiscpConfig);
+  eiscp.on("connect", data => {
+    log("Onkyo - Connected to", data);
+    this.connected = true;
+    eiscp.raw("PWRQSTN");
+    eiscp.raw("SLIQSTN");
+    eiscp.raw("MVLQSTN");
+    // eiscp.raw("AMTQSTN", data => {});
+  });
+
+  eiscp.on("close", () => {
+    log("Onkyo - Connection Closed");
+    this.connected = false;
+  });
+
+  eiscp.on("data", data => {
+    if (Array.isArray(data.command)) {
+      var command = data.command[0];
+    } else {
+      var { command } = data;
+    }
+
+    switch (command) {
+      case "system-power":
+        if (data.argument === "on") {
+          that.powerState = true;
+        } else {
+          that.powerState = false;
+        }
+        log("Power state -", data.argument);
+        break;
+      case "master-volume":
+        that.tvVolume = data.argument;
+        log("Volume state -", data.argument);
+        break;
+      case "audio-muting":
+        if (data.argument === "on") {
+          that.muteState = true;
+        } else {
+          that.muteState = false;
+        }
+        log("Mute state -", data.argument);
+        break;
+      case "input-selector":
+        for (input in this.defaultInputs) {
+          if (this.defaultInputs[input].code === data.iscp_command) {
+            this.currentInput = this.defaultInputs[input].code;
+          }
+        }
+        log("Input State -", this.currentInput);
+        break;
+    }
+  });
+
+  // ****************************************************************************************************
+  // ********** This is for testing remote functionality in the future  *********************************
+  // ****************************************************************************************************
+
+  this.service
+    .getCharacteristic(Characteristic.RemoteKey)
+    .on("set", function(newValue, callback) {
+      switch (newValue) {
+        case 4:
+          code = "AAAAAQAAAAEAAAB0Aw==";
+          log(code);
+          break;
+        case 5:
+          code = "AAAAAQAAAAEAAAB1Aw==";
+          log(code);
+          break;
+        case 6:
+          code = "AAAAAQAAAAEAAAA0Aw==";
+          log(code);
+          break;
+        case 7:
+          code = "AAAAAQAAAAEAAAAzAw==";
+          log(code);
+          break;
+        case 8:
+          code = "AAAAAQAAAAEAAABlAw==";
+          log(code);
+          break;
+        case 9:
+          code = "AAAAAgAAAJcAAAAjAw==";
+          log(code);
+          break;
+        case 15:
+          code = "AAAAAQAAAAEAAABgAw==";
+          log(code);
+          break;
+        case 10:
+          code = "AAAAAgAAAJcAAAAYAw==";
+          log(code);
+          break;
+        case 11:
+          code = "AAAAAgAAAJcAAAAaAw==";
+          log(code);
+          break;
+      }
+
+      xmlcode = code;
+      log(code);
+      callback(null);
+    });
+}
+
+// SETUP SERICES
+
+onkyoAccessory.prototype.prepareInformationService = function() {
+  this.informationService = new Service.AccessoryInformation();
+  this.informationService
+    .setCharacteristic(Characteristic.Manufacturer, "Onkyo")
+    .setCharacteristic(Characteristic.Model, this.name)
+    .setCharacteristic(Characteristic.SerialNumber, "123-456-789")
+    .setCharacteristic(Characteristic.FirmwareRevision, "0.0.1");
+
+  this.services.push(this.informationService);
+};
+
+onkyoAccessory.prototype.prepareTvSpeakerService = function() {
+  this.tvSpeakerService = new Service.TelevisionSpeaker(
+    this.name + " Volume",
+    "tvSpeakerService"
+  );
+  this.tvSpeakerService
+    .setCharacteristic(Characteristic.Active, Characteristic.Active.ACTIVE)
+    .setCharacteristic(
+      Characteristic.VolumeControlType,
+      Characteristic.VolumeControlType.ABSOLUTE
+    );
+  this.tvSpeakerService
+    .getCharacteristic(Characteristic.VolumeSelector)
+    .on("set", (state, callback) => {
+      this.log.debug(
+        "Onkyo - volume change over the remote control (VolumeSelector), pressed: %s",
+        state === 1 ? "Down" : "Up"
+      );
+      this.setVolumeSwitch(state, callback, !state);
+    });
+  this.tvSpeakerService
+    .getCharacteristic(Characteristic.Mute)
+    .on("get", this.getMuteState.bind(this))
+    .on("set", this.setMuteState.bind(this));
+  this.tvSpeakerService
+    .addCharacteristic(Characteristic.Volume)
+    .on("get", this.getVolume.bind(this))
+    .on("set", this.setVolume.bind(this));
+
+  this.services.push(this.tvSpeakerService);
+};
+
+// POWER
+
+onkyoAccessory.prototype.getPowerState = function(callback) {
+  callback(null, this.powerState);
+};
+
+onkyoAccessory.prototype.setPowerState = function(state, callback) {
+  if (state) {
+    eiscp.raw("PWR01");
+    this.log.debug("Onkyo - power service - turned on", this.powerState);
+    callback();
+  } else {
+    if (this.powerState) {
+      eiscp.raw("PWR00");
+      this.log.debug("Onkyo - power service - turned off", this.powerState);
+      this.powerState = false;
+    }
+    callback();
+  }
+};
+
+// MUTE
+
+onkyoAccessory.prototype.getMuteState = function(callback) {
+  callback(null, this.muteState);
+};
+
+onkyoAccessory.prototype.setMuteState = function(state, callback) {
+  if (state) {
+    eiscp.raw("AMTTG");
+    this.log.debug("Onkyo - mute service - toggle", this.powerState);
+    callback();
+  }
+};
+
+// VOLUME
+
+onkyoAccessory.prototype.getVolume = function(callback) {
+  callback(null, this.tvVolume);
+};
+
+onkyoAccessory.prototype.setVolume = function(level, callback) {
+  if (this.powerState) {
+    this.log.debug(
+      "Onkyo - volume service - setting volume to %s, limit: %s",
+      level,
+      this.volumeLimit
+    );
+    if (level > this.volumeLimit) {
+      level = this.volumeLimit;
+    }
+    eiscp.raw("MVL" + level);
+    callback();
+  } else {
+    callback(new Error("Onkyo - is not powered on, cannot set volume"));
+  }
+};
+
+onkyoAccessory.prototype.getVolumeSwitch = function(callback) {
+  callback(null, false);
+};
+
+onkyoAccessory.prototype.setVolumeSwitch = function(state, callback, isUp) {
+  if (this.connected) {
+    this.log.debug(
+      "Onkyo - volume service - volume %s pressed, current volume: %s, limit: %s",
+      isUp ? "Up" : "Down",
+      this.tvVolume,
+      this.volumeLimit
+    );
+    let volLevel = this.tvVolume;
+    if (isUp) {
+      if (volLevel < this.volumeLimit) {
+        eiscp.raw("MVLUP");
+      }
+    } else {
+      eiscp.raw("MVLDOWN");
+    }
+    setTimeout(() => {
+      if (this.volumeUpService)
+        this.volumeUpService
+          .getCharacteristic(Characteristic.On)
+          .updateValue(false);
+      if (this.volumeDownService)
+        this.volumeDownService
+          .getCharacteristic(Characteristic.On)
+          .updateValue(false);
+    }, 10);
+    callback();
+  } else {
+    callback(
+      new Error("Onkyo - volume service - is not connected, cannot set volume")
+    );
+  }
+};
+
+// ****************************************************************************************************
+// ********** This is for testing remote functionality in the future  *********************************
+// ****************************************************************************************************
+
+// REMOTE CONTROL
+
+onkyoAccessory.prototype.getRemoteControlButtonState = function(callback) {
+  callback(null, false);
+};
+
+onkyoAccessory.prototype.setRemoteControlButtonState = function(
+  state,
+  callback,
+  rcButton
+) {
+  if (rcButton === "CLICK") {
+    this.log.debug(
+      "Onkyo - remote control button service - emulating remote control %s press",
+      rcButton
+    );
+  } else {
+    log("NOT CLICK");
+  }
+  callback();
+};
+
+onkyoAccessory.prototype.remoteKeyPress = function(remoteKey, callback) {
+  this.log.debug("Onkyo - remote key pressed: %d", remoteKey);
+
+  switch (remoteKey) {
+    case Characteristic.RemoteKey.REWIND:
+      this.setRemoteControlButtonState(true, callback, "REWIND");
+      break;
+    case Characteristic.RemoteKey.FAST_FORWARD:
+      this.setRemoteControlButtonState(true, callback, "FASTFORWARD");
+      break;
+    case Characteristic.RemoteKey.NEXT_TRACK:
+      // does a endpoint call exist?
+      this.log.info("webOS - next track remote key not supported");
+      callback();
+      break;
+    case Characteristic.RemoteKey.PREVIOUS_TRACK:
+      // does a endpoint call exist?
+      this.log.info("webOS - previous track remote key not supported");
+      callback();
+      break;
+    case Characteristic.RemoteKey.ARROW_UP:
+      this.setRemoteControlButtonState(true, callback, "UP");
+      break;
+    case Characteristic.RemoteKey.ARROW_DOWN:
+      this.setRemoteControlButtonState(true, callback, "DOWN");
+      break;
+    case Characteristic.RemoteKey.ARROW_LEFT:
+      this.setRemoteControlButtonState(true, callback, "LEFT");
+      break;
+    case Characteristic.RemoteKey.ARROW_RIGHT:
+      this.setRemoteControlButtonState(true, callback, "RIGHT");
+      break;
+    case Characteristic.RemoteKey.SELECT:
+      this.setRemoteControlButtonState(true, callback, "ENTER");
+      break;
+    case Characteristic.RemoteKey.BACK:
+      this.setRemoteControlButtonState(true, callback, "BACK");
+      break;
+    case Characteristic.RemoteKey.EXIT:
+      this.setRemoteControlButtonState(true, callback, "EXIT");
+      break;
+    case Characteristic.RemoteKey.PLAY_PAUSE:
+      if (this.isPaused) {
+        this.setRemoteControlButtonState(true, callback, "PLAY");
+      } else {
+        this.setRemoteControlButtonState(true, callback, "PAUSE");
+      }
+      this.isPaused = !this.isPaused;
+      break;
+    case Characteristic.RemoteKey.INFORMATION:
+      this.setRemoteControlButtonState(true, callback, "INFO");
+      break;
+  }
+};
+
+// RUN SERVICE
+
+onkyoAccessory.prototype.getServices = function() {
+  return this.services;
+};
